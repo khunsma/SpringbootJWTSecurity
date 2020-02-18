5a984984b3551e6f58e89500292dea11dddbd0a0
khunsoemoe.aung <khunsma90@gmail.com>
2020-02-18 15:37:12 +0630

    Initialize Project

---
+++ src/main/java/com/nouhoun/springboot/jwt/integration/SpringbootJwtApplication.java
@@ -0,0 +1,14 @@
+package com.nouhoun.springboot.jwt.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//https://medium.com/better-programming/secure-a-spring-boot-rest-api-with-json-web-token-reference-to-angular-integration-e57a25806c50
+
+@SpringBootApplication
+public class SpringbootJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJwtApplication.class, args);
+	}
+}
